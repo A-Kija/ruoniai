1aa8fed0e5860b6bf63f4d76f9603616d6d6df62
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-31 09:53:31 +0300

    boot css

---
+++ r1/src/Components/016/Select.jsx
@@ -0,0 +1,21 @@
+function Select() {
+
+    return (
+        <div className="card m-5">
+
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <select className="form-control">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+        </div>
+    )
+
+}
+
+export default Select;
