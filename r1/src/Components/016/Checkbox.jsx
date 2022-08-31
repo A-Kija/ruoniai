5f9cd2ea6d41e361aa10baa0fa1ed51c32505c01
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-31 10:37:36 +0300

    va cb

--- r1/src/Components/016/Checkbox.jsx
+++ r1/src/Components/016/Checkbox.jsx
@@ -10,16 +10,20 @@ function Checkbox() {
                 <h5 className="card-title">CHECKBOX</h5>
                 <div>
                     <div className="form-check m-4">
-                        <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                        <label className="form-check-label" HtmlFor="inlineCheckbox1">1</label>
+                        <input className="form-check-input" type="checkbox" id="_1" value="A" />
+                        <label className="form-check-label mt-2" htmlFor="_1">Raidė A</label>
                     </div>
                     <div className="form-check m-4">
-                        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                        <label className="form-check-label" HtmlFor="inlineCheckbox2">2</label>
+                        <input className="form-check-input" type="checkbox" id="_2" value="B" />
+                        <label className="form-check-label mt-2" htmlFor="_2">Raidė B</label>
                     </div>
                     <div className="form-check m-4">
-                        <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
-                        <label className="form-check-label" HtmlFor="inlineCheckbox3">3 (disabled)</label>
+                        <input className="form-check-input" type="checkbox" id="_3" value="C" />
+                        <label className="form-check-label mt-2" htmlFor="_3">Raidė C</label>
+                    </div>
+                    <div className="form-check m-4">
+                        <input className="form-check-input" type="checkbox" id="_4" value="D" />
+                        <label className="form-check-label mt-2" htmlFor="_4">Raidė D</label>
                     </div>
                 </div>
             </div>
