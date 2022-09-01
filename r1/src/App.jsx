3b28a9811a984e951d86c174bed763e6d481886b
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-01 10:16:20 +0300

    cb

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -2,17 +2,23 @@ import './App.scss';
 import Text from './Components/017/Text';
 import { useState } from "react";
 import Show from './Components/017/Show';
+import Cb from './Components/017/Cb';
 
 function App() {
 
     const [showText, setShowText] = useState('');
+    const [color, setColor] = useState(false);
 
     return (
         <div className="App">
             <header className="App-header">
+                <div className="dog-bin">
+                    <div className="dog" style={{backgroundColor: color ? 'crimson' : 'black'}}></div>
+                </div>
                 <h1>State Uplifting</h1>
                 <Text setShowText={setShowText} />
                 <Show showText={showText} />
+                <Cb setColor={setColor} />
             </header>
         </div>
     );
