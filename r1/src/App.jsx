4e5c8b9029ca836641fe7113bbf6f566f5f90574
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-05 10:52:32 +0300

    cache

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,26 +1,48 @@
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 import './App.scss';
-import Squares from './Components/017/Squares';
-// import Text from './Components/017/Text';
-// import { useState } from "react";
-// import Show from './Components/017/Show';
-// import Cb from './Components/017/Cb';
-
 function App() {
 
-    // const [showText, setShowText] = useState('');
-    // const [color, setColor] = useState(false);
+    const [news, setNews] = useState(null);
+    const time = useRef(0);
+
+    useEffect(() => {
+        axios.get('https://in3.dev/vinted/api/news/')
+        .then(res => setNews(res.data));
+    }, []);
+
+    useEffect(() => {
+        if (null === news) {
+            return;
+        }
+        if (time.current === 1) {
+            return;
+        }
+        time.current++;
+        news.forEach(n => {
+            let store = localStorage.getItem('vinted_' + n.id);
+            if (store !== null) {
+                store = JSON.parse(store);
+                setNews(n1 => n1.map(n2 => n2.id === n.id ? store : {...n2}));
+            } else {
+                axios.get('https://in3.dev/vinted/api/products/' + n.id)
+                .then(res => {
+                    setNews(n1 => n1.map(n2 => n2.id === n.id ? res.data : {...n2}));
+                    localStorage.setItem('vinted_' + n.id, JSON.stringify(res.data));
+                });
+            }
+        });
+    }, [news]);
+
+
+
+
 
     return (
         <div className="App">
             <header className="App-header">
-                {/* <div className="dog-bin">
-                    <div className="dog" style={{backgroundColor: color ? 'crimson' : 'black'}}></div>
-                </div> */}
-                <h1>State Uplifting AND useRef</h1>
-                {/* <Text setShowText={setShowText} />
-                <Show showText={showText} />
-                <Cb setColor={setColor} /> */}
-                <Squares />
+
+
             </header>
         </div>
     );
