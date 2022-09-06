f05198febf639fe16add0e3474f984a7817fadde
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-06 10:30:46 +0300

    reduceris

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,94 +1,53 @@
-import { useEffect, useState, useRef } from 'react';
-import axios from 'axios';
-import rand from './Functions/rand';
+import { useReducer } from 'react';
+import { useState } from 'react';
 import './App.scss';
+import StarContext from './Components/019/StarContext';
+import countReducer from './Reducers/countReducer';
+// import Tevelis from './Components/019/Tevelis';
+
 function App() {
 
-    const [news, setNews] = useState(null);
-    const time = useRef(0);
+    const [star, setStar] = useState('');
+    const [plus, setPlus] = useState('');
 
-    useEffect(() => {
-        axios.get('https://in3.dev/vinted/api/news/')
-            .then(res => setNews(res.data));
-    }, []);
+    const [count, countDispach] = useReducer(countReducer, 10);
 
-    useEffect(() => {
-        if (null === news) {
-            return;
+
+    const add1 = () => {
+        const action = {
+            type: 'add_one'
         }
-        if (time.current === 1) {
-            return;
-        }
-        time.current++;
-        news.forEach(n => {
-            let store = localStorage.getItem('vinted_' + n.id);
-            if (store !== null) {
-                store = JSON.parse(store);
-                setNews(n1 => n1.map(n2 => n2.id === n.id ? store : { ...n2 }));
-            } else {
-                axios.get('https://in3.dev/vinted/api/products/' + n.id)
-                    .then(res => {
-                        setNews(n1 => n1.map(n2 => n2.id === n.id ? res.data : { ...n2 }));
-                        localStorage.setItem('vinted_' + n.id, JSON.stringify(res.data));
-                    });
-            }
-        });
-    }, [news]);
-
-
-    const [kv, setKv] = useState(null);
-    const undo = useRef([]);
-
-    useEffect(() => {
-        const k = localStorage.getItem('kv');
-        if (null === k) {
-            setKv([]);
-        } else {
-            setKv(JSON.parse(k));
-        }
-    }, []);
-
-    useEffect(() => {
-        if (null === kv) {
-            return;
-        }
-        localStorage.setItem('kv', JSON.stringify(kv));
-        undo.current.push(kv);
-    }, [kv]);
-
-
-    const add = () => {
-        setKv(k => [...k ?? [], ...[...Array(rand(5, 10))]]);
+        countDispach(action);
     }
 
-    const rem = () => {
-        setKv([]);
-    }
-
-    const un = () => {
-        const first = undo.current.pop();
-        if(!first) {
-            return;
+    const rem1 = () => {
+        const action = {
+            type: 'remove_one'
         }
-        if (first.length === kv.length) {
-            setKv(undo.current.pop());
-        } else {
-            setKv(first);
-        }
+        countDispach(action);
     }
 
     return (
         <div className="App">
+            <StarContext.Provider value={
+                {
+                    star: star,
+                    plus: plus,
+                    setPlus,
+                    setStar
+                }
+            }>
             <header className="App-header">
-                <div className="dog-bin">
-                    {
-                        kv?.map((_, i) => <div className="dog" key={i}></div>)
-                    }
+                <h1>CONTEX + REDUCER {count}</h1>
+                <div className="red-square">
+                {/* <button onClick={() => setStar(s => s + '*')}>Make Star</button>
+                <button onClick={() => setPlus(s => s + '+')}>Make Plus</button>
+                <Tevelis/> */}
+                <button onClick={add1}>+1</button>
+                <button onClick={rem1}>-1</button>
                 </div>
-                <button onClick={add}>ADD</button>
-                <button onClick={rem}>REMOVE</button>
-                <button onClick={un}>UNDO</button>
             </header>
+            </StarContext.Provider>
         </div>
     );
 }
