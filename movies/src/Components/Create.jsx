f51281c73068cbacc1a4ea6ef93f8a6bea01aa42
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-07 09:56:29 +0300

    w font

--- movies/src/Components/Create.jsx
+++ movies/src/Components/Create.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import genres from '../Data/genres';
 
 function Create() {
+
+    const [title, setTitle] = useState('');
+    const [genre, setGenre] = useState('0');
+    const [year, setYear] = useState('');
 
     return (
         <div className="card m-4">
@@ -8,11 +13,12 @@ function Create() {
             <div className="card-body">
                 <div className="mb-3">
                     <label className="form-label">Movie Title</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" value={title} onChange={e => setTitle(e.target.value)} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Genre</label>
-                    <select className="form-select">
+                    <select className="form-select" value={genre} onChange={e => setGenre(e.target.value)}>
+                        <option value={0} disabled>Choose from list</option>
                         {
                             genres.map(g => <option key={g.id} value={g.id}>{g.type}</option>)
                         }
@@ -20,7 +26,7 @@ function Create() {
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Movie Year</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" value={year} onChange={e => setYear(e.target.value)} />
                 </div>
                 <button type="button" className="btn btn-outline-success">Add</button>
             </div>
