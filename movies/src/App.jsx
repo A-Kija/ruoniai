40055d65d25838d414f3ea48062e3a6a1d3035a5
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-07 09:21:49 +0300

    done

---
+++ movies/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-5">
+          Column
+        </div>
+        <div className="col-7">
+          Column
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
