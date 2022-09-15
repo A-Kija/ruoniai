2059ae949e027ac1a37037351acd1d8144ffc458
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-15 09:45:56 +0300

    print state

--- r1/src/Components/025/Buttons.jsx
+++ r1/src/Components/025/Buttons.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 function Buttons() {
+
+    const [number, setNumber] = useState(0);
 
     return (
         <>
+        <h2>{number}</h2>
         <div className="dog-bin">
             {
-                [...Array(50)].map((_, i) => <button key={i} onClick={() => console.log('Va')}>{i + 1}</button>)
+                [...Array(50)].map((_, i) => <button key={i} onClick={() => setNumber(i + 1)}>{i + 1}</button>)
             }
         </div>
         </>
