1ddc323c32ccb9f11aae42553017e2376e248d6f
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-19 10:15:02 +0300

    create

--- server/app.js
+++ server/app.js
@@ -56,6 +56,8 @@ app.get('/super/cool', (req, res) => {
 //     });
 // });
 
+
+// READ
 app.get("/trees", (req, res) => {
     const sql = `
     SELECT id, type, title, height
@@ -67,6 +69,9 @@ app.get("/trees", (req, res) => {
     });
 });
 
+//CREATE
+// INSERT INTO table_name (column1, column2, column3, ...)
+// VALUES (value1, value2, value3, ...);
 
 
 app.listen(port, () => {
