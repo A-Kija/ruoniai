9a20db7701b4891f62f7222ed009b5dd1a524c53
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-21 10:15:39 +0300

    Nav

---
+++ et/front/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import { BrowserRouter } from 'react-router-dom';
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Nav/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
