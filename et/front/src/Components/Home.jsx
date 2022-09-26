36b5fcf9faa17d168a6be4dccef3a5d4fd34b163
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-26 09:43:48 +0300

    push

--- et/front/src/Components/Home.jsx
+++ et/front/src/Components/Home.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
+import randColor from '../Functions/randColor';
+
 function Home() {
+
+    const [fill, setFill] = useState(null);
+
+    useEffect(() => {
+        const timerId = setInterval( () => {
+            setFill(randColor());
+        }, 3000);
+        return () => {
+            clearInterval(timerId);
+        }
+    }, []);
+
     return (
         <div className="container">
         <div className="row">
             <div className="col-12">
                 <div className="image">
-                <svg><use xlinkHref="#electro" ></use></svg>
+                <svg style={{fill}}><use xlinkHref="#electro" ></use></svg>
+                <svg style={{fill}}><use xlinkHref="#electro" ></use></svg>
+                <svg style={{fill}}><use xlinkHref="#electro" ></use></svg>
                 </div>
             </div>
         </div>
